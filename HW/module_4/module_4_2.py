74780d782880db891e75a37f5389613c04e2863c
YouRA <PISMO29@YANDEX.RU>
2024-11-30 21:01:50 +0300

    4.2. Домашняя работа по уроку "Пространство имен." Доработанный код без вывода ошибки

--- HW/module_4/module_4_2.py
+++ HW/module_4/module_4_2.py
@@ -4,7 +4,8 @@ def test_function():
 
     inner_function()
 
-
-inner_function
-# При попытке вызвать inner_function вне test_function, получим ошибку,
-# так как inner_function определена только внутри test_function и не существует вне её области видимости.
+test_function()
+# При вызове inner_function() ничего не выводится, потому что
+# При попытке вызвать inner_function вне test_function, получим ошибку (NameError: name 'inner_function' is not defined.
+# Did you mean: 'test_function'?), # так как inner_function определена только внутри test_function
+# и не существует вне её области видимости.
